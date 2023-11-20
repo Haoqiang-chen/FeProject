c11781a9215cb5ad8067564797b8ec423430af18
chenhaoqiang <chenhaoqiang@baidu.com>
2023-11-20 17:34:25 +0800

    js基础2

--- src/js/basic.js
+++ src/js/basic.js
@@ -18,15 +18,71 @@ function clickToGrammar(id) {
     var aB = "这是字符串";
     var element = document.getElementById(id);
     element.innerHTML = ab + aB;
+
     // js中的数据类型分为基本类型和引用类型
     // 基本类型：String(字符床)、Number(数字)、Boolean(布尔)、Null（空）、Undefined(未定义，当声明了一个变量但是没有赋值时，该变量就是undefined)、Symbol(ES6新增)
-    // 引用类型：Object(对象)、Array(数组)、Function(函数)、RegExp(正则)、Date（日期）
+    // 引用类型：Object(对象)、Array(数组)、Function(函数)、RegExp(正则)、Date（日期）等
     var object = {
         name: "张三",
         age: 18,
+        // 对象中的方法
         method: function () {
             return "学生：" + this.name + ", 年纪：" + this.age;
         }
     };
-    element.innerHTML = object.method();
+
+    // 类型判断 typeof 可以用于判断出变量的类型
+    var type = typeof object
+    element.innerHTML = object.method() + `  类型为：${type}`;
+
+    // 构造器属性 constructor可以获取变量的构造器
+    console.log(object.constructor);
+    console.log(["1"].constructor);
+    console.log("Str".constructor);
+}
+
+/*js验证表单数据*/
+function checkForm() {
+    var num = document.forms["testForm"]["num"].value;
+    if (num < 0 || num > 10) {
+        alert("请输入0-10之间的数字");
+    }
+}
+
+/*AJAX异步请求*/
+function asyncRequest(id) {
+    var asyncRequest = new XMLHttpRequest();
+    asyncRequest.open("GET", "https://www.runoob.com/try/ajax/demo_test.php");
+    asyncRequest.onload = function () {
+        document.getElementById(id).innerHTML = `请求成功：${asyncRequest.responseText}`;
+    }
+    asyncRequest.onerror = function () {
+        document.getElementById(id).innerHTML = `请求失败:${asyncRequest.status}`;
+    }
+    asyncRequest.send();
+}
+
+/*Promise异步请求*/
+function asyncRequestByPromise(id) {
+    var asyncRequestByPromise = new Promise(function (resolve, reject) {
+        var asyncRequest = new XMLHttpRequest();
+        asyncRequest.open("GET", "https://www.runoob.com/try/ajax/demo_test.php");
+        asyncRequest.onload = function () {
+            resolve(`请求成功：${asyncRequest.responseText}`);
+        }
+        asyncRequest.onerror = function () {
+            reject(`请请求失败:${asyncRequest.status}`)
+        }
+        asyncRequest.open("GET", "https://www.runoob.com/try/ajax/demo_test.php");
+        asyncRequest.send();
+    })
+    asyncRequestByPromise.then(function (value) {
+        console.log("请求成功");
+        document.getElementById(id).innerHTML = value;
+    }).catch(function (error) {
+        console.log("请求失败");
+        document.getElementById(id).innerHTML = error;
+    }).finally(function () {
+        console.log("请求结束");
+    })
 }
