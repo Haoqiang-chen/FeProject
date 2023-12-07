60be636e0a0a0647cf806ac05d4eb3091656a902
chenhaoqiang <chenhaoqiang@baidu.com>
2023-12-07 10:30:11 +0800

    js基础类型学习

--- src/js/data_type.js
+++ src/js/data_type.js
@@ -22,3 +22,25 @@ function baseDataType() {
     console.log("对象的字符串表示：" + object.valueOf())
 
 }
+
+/**
+ * 基础引用类型
+ */
+function baseReferenceType() {
+    // 时间类型
+    let date = new Date()
+    console.log(date.toDateString())
+
+    // String、Number、Boolean对象
+    // 当时用字符串、数值、布尔的字面量时，其实都会在后台创建一个对应的包装器类型，然后用完包装器类型对象就会销毁
+    // 比如下面的代码就相当于
+    // let str = new String("This is a Str")
+    // let str1 = str.substring(5)
+    // str = null
+    let str = "This is a Str"
+    let str1 = str.substring(5)
+    console.log(str1)
+
+    // 内置对象
+
+}
